47cf47acee8848f2945fa59f0c9a5e2369d492f5
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2019-06-05 18:56:27 -0400

    Initial commit

---
+++ jumpdir.rb
@@ -0,0 +1,19 @@
+require 'fileutils'
+
+if !File.exist?("#{ENV['HOME']}/.local/share/jr/data.txt")\
+    || ARGV.length != 1
+  puts Dir.pwd
+  exit
+end
+
+File.open("#{ENV['HOME']}/.local/share/jr/data.txt") do |f|
+  f.each_line do |line|
+    dir, = line.split
+    if /#{ARGV[0]}/ =~ dir.downcase
+      puts dir
+      exit
+    end
+  end
+end
+
+puts Dir.pwd
