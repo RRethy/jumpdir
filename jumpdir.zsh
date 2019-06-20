169cd54ab3ba923a6866980c61326c881a15cd81
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2019-06-20 09:25:38 -0400

    Fix incorrect script path

--- jumpdir.zsh
+++ jumpdir.zsh
@@ -4,28 +4,30 @@ compdef _jd jd
 
 chpwd_functions+=(storepwd)
 
+dir=$(dirname "$0")
+
 function storepwd {
-  ruby ~/ruby/jumpdir/jumpdir.rb --incdir $(pwd)
+  ruby ${dir}/jumpdir.rb --incdir $(pwd)
 }
 
 function jc {
   if [[ -n "$1" ]]; then
-    cd $(ruby ~/ruby/jumpdir/jumpdir.rb --jumpchild $1)
+    cd $(ruby ${dir}/jumpdir.rb --jumpchild $1)
   fi
 }
 
 function jd {
-  cd $(ruby ~/ruby/jumpdir/jumpdir.rb --jumpdir $1)
+  cd $(ruby ${dir}/jumpdir.rb --jumpdir $1)
 }
 
 function jm {
-  cd $(ruby ~/ruby/jumpdir/jumpdir.rb --jumpmark $1)
+  cd $(ruby ${dir}/jumpdir.rb --jumpmark $1)
 }
 
 function m {
-  ruby ~/ruby/jumpdir/jumpdir.rb --markdir $1
+  ruby ${dir}/jumpdir.rb --markdir $1
 }
 
 function _jd {
-  compadd -U $(ruby ~/ruby/jumpdir/jumpdir.rb --complete $PREFIX)
+  compadd -U $(ruby ${dir}/jumpdir.rb --complete $PREFIX)
 }
